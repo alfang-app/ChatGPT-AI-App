2f46b55098fd57baf45ccc02d4d6ac00e21b3616
alvin480 <kpfang@outlook.com>
2022-12-24 18:44:15 -0800

    add all

--- server/server.js
+++ server/server.js
@@ -3,15 +3,13 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config()
-process.env.OPENAI_API_KEY="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";
-
+dotenv.config() 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 
-//console.log("???" + process.env.OPENAI_API_KEY);
+console.log("???" + process.env.OPENAI_API_KEY);
 const openai = new OpenAIApi(configuration);
 
 const app = express()
