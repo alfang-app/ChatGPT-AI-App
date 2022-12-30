20ff1ca38eae3621e1cf4cb65ea0b7836ad3e8db
alvin480 <kpfang@outlook.com>
2022-12-29 22:39:11 -0800

    update url fetch post

--- client/script.js
+++ client/script.js
@@ -88,7 +88,9 @@ const handleSubmit = async (e) => {
 
     // TODO MODIFY TO ACTUAL LINK
     //'https://codex-im0y.onrender.com/'
-    const response = await fetch('http://localhost:5000/', {
+    //https://chatgptservice.onrender.com/
+    // http://localhost:5000/
+    const response = await fetch('https://chatgptservice.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
